b87c561d771fba14c78a62c172a62ba01cb10aed
Shivam Bhardwaj <bshiam472@gmail.com>
2024-01-20 19:22:07 +0530

    episode 1 finish marked

--- Reactjs/App1.js
+++ Reactjs/App1.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1",{},"this is coming from react");
+const heading = React.createElement("h1",{id:"heading1"},"this is coming from react");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
